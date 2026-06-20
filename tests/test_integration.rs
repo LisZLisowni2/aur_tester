7837cc03d599971e884ce78c4a3cdde0b872ce63
lislisowni2 <rafalkozikowski735@gmail.com>
2026-06-20 11:25:52 +0200

    Update integration tests

--- tests/test_integration.rs
+++ tests/test_integration.rs
@@ -1,11 +1,7 @@
 #[cfg(test)]
-mod test {
+mod integration_tests {
     use std::collections::HashSet;
-    use std::fs::{self, File};
-    use std::io::Write;
-    use std::net::Ipv4Addr;
     use tokio::sync::mpsc;
-    use std::time::Duration;
     use bollard::config::ContainerCreateBody;
     use futures_util::TryStreamExt;
     use aur_tester::{command, sniffer};
@@ -50,37 +46,11 @@ mod test {
 
         Some(id)
     }
-//
-//     fn setup_malicious_pkgbuild(dir_path: &str) -> std::io::Result<()> {
-//         fs::create_dir_all(dir_path)?;
-//         let file_path = format!("{}/PKGBUILD", dir_path);
-//         let mut file = File::create(file_path)?;
-//
-//         let content = r#"
-// pkgname=test-malware
-// pkgver=1.0.0
-// pkgrel=1
-// arch=('any')
-// source=("https://github.com/archlinux/yay")
-// sha256sums=('SKIP')
-//
-// prepare() {
-//     curl -s https://www.wikipedia.org > /dev/null
-// }
-// "#;
-//         file.write_all(content.as_bytes())?;
-//         Ok(())
-//     }
 
     #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
     async fn test_sniffer_detects_unauthorized_domain() {
         const IMAGE: &str = "archlinux:latest";
-
-        // let test_dir = "/tmp/aur_test_malware";
-
-        // setup_malicious_pkgbuild(test_dir).expect("Could not setup malware");
-        //
-        // let pkgbuild_path = format!("{}/PKGBUILD", test_dir);
+        
         let mut allowed_domains = HashSet::new();
 
         allowed_domains.insert("aur.archlinux.org".to_string());
@@ -148,7 +118,6 @@ prepare() {
 
         let _ = docker.remove_container(&id, Some(bollard::query_parameters::RemoveContainerOptions { force: true, ..Default::default() })).await;
         sniffer_handle.abort();
-        // let _ = fs::remove_dir_all(test_dir);
 
         assert!(test_result, "Sniffer doesn't detect connection to wikipedia.org!");
     }
